535b725246fd50556adaf516d35894aee525301e
kojikoji777 <kojikoji777@example.com>
2015-07-27 23:56:02 +0900

    Ch05 SELECTの基本的な使い方

--- cakephp/app/Controller/TasksController.php
+++ cakephp/app/Controller/TasksController.php
@@ -43,4 +43,53 @@ class TasksController extends AppController {
 		}
 		$this->render("create");
 	}
+
+	public function select(){
+		//全件　select * from tasks
+// 		$options = array();
+
+		//条件付き　select * from tasks where status < 0 order by id DESC limit 2
+// 		$options = array(
+// 			"conditions" => array(
+// 				"status >" => 0
+// 			),
+// 			"order" => "id DESC",
+// 			"limit" => 2
+// 		);
+
+		//条件付き　select * from tasks where (status = 1 OR name IN ('TEST', 'TEST1'))
+// 		$options = array(
+// 				"conditions" => array(
+// 					"OR" => array(
+// 						"status" => 1,
+// 						"name" => array("TEST", "TEST1")
+// 					)
+// 				),
+// 		);
+// 		$taskList = $this->Task->find("all", $options);
+// 		debug($taskList);
+
+		//件数取得　select xount(*) from tasks
+		//条件の付け方は通常のSELECT文と同じ
+// 		$count = $this->Task->find("count", $options);
+// 		debug($count);
+
+		//最初の1件取得 select * from tasks WHERE id = 1
+// 		$options = array(
+// 				"conditions" => array(
+// 					"id " => 1
+// 				),
+// 		);
+// 		$taskData = $this->Task->find("first", $options);
+// 		debug($taskData);
+
+		//マジックfind
+// 		$taskData = $this->Task->findById(1);
+// 		$taskData = $this->Task->findAllByStatus(0);
+
+		//queryメソッド(直接SQLを叩く)
+		$sql = "SELECT * FROM tasks WHERE id IN(1,3,5)";
+		$list = $this->Task->query($sql);
+		debug($list);
+	}
 }
